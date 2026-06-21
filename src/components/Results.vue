583a76483bf5bfd20d685a9d95465040bf8ce593
George Patistas <georgepatistas13@gmail.com>
2026-06-21 17:33:08 +0300

    Vue Reaction Timer Project

---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = 'God hands'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflaxes'
+        } else {
+            this.rank = 'Dogshit :('
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: darkgoldenrod;
+        font-weight: bold;
+    }
+
+</style>
